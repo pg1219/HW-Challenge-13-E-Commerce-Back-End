1c6cfa7379bf87e396af17bf38f358dd1ad33461
pg1219 <peter.gronseth@gmail.com>
2023-05-29 21:40:10 -0400

    monday night push

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -101,8 +101,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const products = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!products) {
+      res.status(404).json({ message: "No product found with this ID" });
+      return;
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
